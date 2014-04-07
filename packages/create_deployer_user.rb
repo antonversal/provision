72788833ecc0230f365f42c85ce0f26d307632b6
Anton Versal <ant.ver@gmail.com>
2014-04-07 14:36:28 +0300

    few changes

--- packages/create_deployer_user.rb
+++ packages/create_deployer_user.rb
@@ -1,8 +1,8 @@
 package :create_deployer_user do
   # create basic ssh key for all users - this is unsafe and does not work or every linux distro
-  # runner 'mkdir -p /etc/skel/.ssh'
-  # id_rsa_pub = `cat ~/.ssh/id_rsa.pub`.strip
-  # runner "sh -c \"echo '#{id_rsa_pub}' > /etc/skel/.ssh/authorized_keys\""
+  runner 'mkdir -p /etc/skel/.ssh'
+  id_rsa_pub = `cat ~/.ssh/id_rsa.pub`.strip
+  runner "sh -c \"echo '#{id_rsa_pub}' > /etc/skel/.ssh/authorized_keys\""
 
   # create deployer user
   runner 'useradd -m -s /bin/bash deployer'
